1104bdcdc646156c4628a44b514d2939b03bd59e
twicetw <nadiaramadhani919@gmail.com>
2022-05-12 15:16:04 +0700

    Initial Commit

---
+++ MC1 ACTION SHEET/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC1 ACTION SHEET
+//
+//  Created by Nadia Ramadhani on 12/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MC1 ACTION SHEET/MC1_ACTION_SHEETApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC1_ACTION_SHEETApp.swift
+//  MC1 ACTION SHEET
+//
+//  Created by Nadia Ramadhani on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MC1_ACTION_SHEETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
